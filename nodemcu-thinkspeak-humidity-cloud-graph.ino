be3d23214b1afdbacff0b8a07a3a7c2530c9e423
BILAL AHMED KHAN <86778416+bilal278khan@users.noreply.github.com>
2021-11-05 15:51:02 +0500

    Update nodemcu-thinkspeak-humidity-cloud-graph.ino

--- nodemcu-thinkspeak-humidity-cloud-graph.ino
+++ nodemcu-thinkspeak-humidity-cloud-graph.ino
@@ -31,7 +31,7 @@ void loop() {
   float t = dht.readTemperature();
   Serial.println("Temperature: " + (String) t);
   Serial.println("Humidity: " + (String) h);
-  ThingSpeak.writeField(myChannelNumber, 1, t, myWriteAPIKey);
+  ThingSpeak.writeField(myChannelNumber, 1, t, myWriteAPIKey);  //function used to send data
   ThingSpeak.writeField(myChannelNumber, 2, h, myWriteAPIKey);
   delay(2000);
 }
